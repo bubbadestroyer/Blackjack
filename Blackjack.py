84c6dd3f8999f31c739ca5e9ef2a992377076488
Egor <godenzo@vk.com>
2022-07-26 19:42:12 +0300

    Add Class Card

--- Blackjack.py
+++ Blackjack.py
@@ -20,3 +20,12 @@ values = {
 }
 
 playing = True
+
+
+class Card:
+    def __init__(self,suit,rank):
+        self.suit = suit
+        self.rank = rank
+    def __str__(self):
+        return f'{self.suit} {self.rank}'
+    
